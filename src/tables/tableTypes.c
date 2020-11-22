293c81466afeadc334acf6f1750880497c0d614f
Aed3r <xxgoffi22xx@gmail.com>
2020-11-22 20:55:15 +0100

    Fix table types

--- src/tables/tableTypes.c
+++ src/tables/tableTypes.c
@@ -1,10 +1,11 @@
 #include "tableTypes.h"
 
-tableType tt_init () {
+int tt[T_TABLETYPES];
+int longTabType = 0;
+
+void tt_init () {
     int i;
-    tableType tt = allocation_mem(T_TABLETYPES, sizeof(int));
     for(i = 0; i < T_TABLETYPES; i++) tt[i] = -1;
-    return tt;
 }
 
 /* Fonction locale permettant de vérifier si t valeurs rentre dans une table des types.
@@ -16,15 +17,15 @@ void verifTaille (int t) {
     }
 }
 
-/* Fonction locale permettant l'ajout de nb valeurs à tt de longueur longTabType.
+/* Fonction locale permettant l'ajout de nb valeurs à la table des types.
    Renvoie la position des données ajoutées */
-int ajoutsValTab (tableType tt, int *longTabType, int nb, va_list vl) {
-    int i, start = *longTabType;
+int ajoutsValTab (int nb, va_list vl) {
+    int i, start = longTabType;
 
-    verifTaille(*longTabType + nb);
+    verifTaille(longTabType + nb);
 
     for (i = 0; i < nb; i++) {
-        tt[(*longTabType)++] = va_arg(vl, int);
+        tt[longTabType++] = va_arg(vl, int);
     }
 
     va_end(vl);
@@ -40,14 +41,14 @@ int ajoutsValTab (tableType tt, int *longTabType, int nb, va_list vl) {
     - le numéro lexicographique du nom du champ
     - le déplacement à l'intérieur de la structure
    La fonction renvoit l'emplacement des données dans la table */
-int tt_ajoutStruct(tableType tt, int *longTabType, int nbChamps, ...) {
+int tt_ajoutStruct(int nbChamps, ...) {
     va_list vl;
 
-    verifTaille(*longTabType+1);
-    tt[(*longTabType)++] = nbChamps;
+    verifTaille(longTabType+1);
+    tt[longTabType++] = nbChamps;
 
     va_start(vl, nbChamps);
-    return ajoutsValTab(tt, longTabType, nbChamps * 3, vl)-1;
+    return ajoutsValTab(nbChamps * 3, vl)-1;
 }
 
 /* Ajoute les informations concernant un tableau (type des éléments: typeElem, nombre de dimensions: 
@@ -55,16 +56,16 @@ int tt_ajoutStruct(tableType tt, int *longTabType, int nbChamps, ...) {
    Les derniers paramètres de la fonction correspondent à nbDimensions fois deux entiers indiquants,
    pour chaque dimension la borne inférieure et la borne supérieure.
    La fonction renvoit l'emplacement des données dans la table */
-int tt_ajoutTab(tableType tt, int *longTabType, int typeElem, int nbDimensions, ...) {
+int tt_ajoutTab(int typeElem, int nbDimensions, ...) {
     va_list vl;
 
-    verifTaille(*longTabType + 2);
+    verifTaille(longTabType + 2);
 
-    tt[(*longTabType)++] = typeElem;
-    tt[(*longTabType)++] = nbDimensions;
+    tt[longTabType++] = typeElem;
+    tt[longTabType++] = nbDimensions;
 
     va_start(vl, nbDimensions);
-    return ajoutsValTab(tt, longTabType, nbDimensions*2, vl)-2;
+    return ajoutsValTab(nbDimensions*2, vl)-2;
 }
 
 /* Ajoute les informations concernant une fonction (type du résultat: typeRes, nombre de paramètres:
@@ -74,16 +75,16 @@ int tt_ajoutTab(tableType tt, int *longTabType, int typeElem, int nbDimensions, 
     - Son numéro lexicographique
     - Son type 
    La fonction renvoit l'emplacement des données dans la table */
-int tt_ajoutFonction(tableType tt, int *longTabType, int typeRes, int nbParams, ...) {
+int tt_ajoutFonction(int typeRes, int nbParams, ...) {
     va_list vl;
 
-    verifTaille(*longTabType + 2);
+    verifTaille(longTabType + 2);
 
-    tt[(*longTabType)++] = typeRes;
-    tt[(*longTabType)++] = nbParams;
+    tt[longTabType++] = typeRes;
+    tt[longTabType++] = nbParams;
 
     va_start(vl, nbParams);
-    return ajoutsValTab(tt, longTabType, nbParams*2, vl)-2;
+    return ajoutsValTab(nbParams*2, vl)-2;
 }
 
 /* Ajoute les informations concernant une procédure (nombre de paramètres: nbParams) à la table de 
@@ -92,77 +93,91 @@ int tt_ajoutFonction(tableType tt, int *longTabType, int typeRes, int nbParams, 
     - Son numéro lexicographique
     - Son type 
    La fonction renvoit l'emplacement des données dans la table */
-int tt_ajoutProcedure(tableType tt, int *longTabType, int nbParams, ...) {
+int tt_ajoutProcedure(int nbParams, ...) {
     va_list vl;
 
-    verifTaille(*longTabType + 1);
+    verifTaille(longTabType + 1);
 
-    tt[(*longTabType)++] = nbParams;
+    tt[longTabType++] = nbParams;
 
     va_start(vl, nbParams);
-    return ajoutsValTab(tt, longTabType, nbParams*2, vl)-1;
+    return ajoutsValTab(nbParams*2, vl)-1;
 }
 
 
+/* Fonction locale vérifiant l'indice pos du tableau des types. 
+   Si ce dernier se trouve dans le tableau la valeur associé est renvoyé */
+int getVal (int pos) {
+    verifTaille(pos);
+    return tt[pos];
+}
 
 /* Renvoie le nombre de champs de la structure se trouvant à posStruct */
-int tt_structNbChamps (tableType tt, int posStruct) { return tt[posStruct]; }
+int tt_structNbChamps (int posStruct) { return getVal(posStruct); }
 
 /* Renvoie l'index du champs i (indexé à 0) de la structure se trouvant à posStruct */
-int tt_structIndexChamp (tableType tt, int posStruct, int i) { return tt[posStruct+1+i*3]; }
+int tt_structIndexChamp (int posStruct, int i) { return getVal(posStruct+1+i*3); }
 
 /* Renvoie le numéro lexicographique du nom du champs i (indexé à 0) de la structure 
    se trouvant à posStruct */
-int tt_structNumLexChamp (tableType tt, int posStruct, int i) { return tt[posStruct+2+i*3]; }
+int tt_structNumLexChamp (int posStruct, int i) { return getVal(posStruct+2+i*3); }
 
 /* Renvoie le déplacement du champs i (indexé à 0) à l'intérieur de la structure se 
    trouvant à posStruct */
-int tt_structDeplacementChamp (tableType tt, int posStruct, int i) { return tt[posStruct+3+i*3]; }
+int tt_structDeplacementChamp (int posStruct, int i) { return getVal(posStruct+3+i*3); }
 
 
 
 /* Renvoie le type des éléments du tableau se trouvant à posTab */
-int tt_tabTypeElem (tableType tt, int posTab) { return tt[posTab]; }
+int tt_tabTypeElem (int posTab) { return getVal(posTab); }
 
 /* Renvoie le nombre de dimensions du tableau se trouvant à posTab */
-int tt_tabNbDimensions (tableType tt, int posTab) { return tt[posTab+1]; }
+int tt_tabNbDimensions (int posTab) { return getVal(posTab+1); }
 
 /* Renvoie la borne inférieure de la dimension i (indexé à 0) du tableau se trouvant à posTab */
-int tt_tabDimBornInf (tableType tt, int posTab, int i) { return tt[posTab+2+i*2]; }
+int tt_tabDimBornInf (int posTab, int i) { return getVal(posTab+2+i*2); }
 
 /* Renvoie la borne supérieure de la dimension i (indexé à 0) du tableau se trouvant à posTab */
-int tt_tabDimBornSup (tableType tt, int posTab, int i) { return tt[posTab+3+i*2]; }
+int tt_tabDimBornSup (int posTab, int i) { return getVal(posTab+3+i*2); }
 
 
 
 /* Renvoie le type de retour de la fonction se trouvant à posFonc */
-int tt_foncTypeRetour (tableType tt, int posFonc) { return tt[posFonc]; }
+int tt_foncTypeRetour (int posFonc) { return getVal(posFonc); }
 
 /* Renvoie le nombre de paramètres de la fonction se trouvant à posFonc */
-int tt_foncNbParam (tableType tt, int posFonc) { return tt[posFonc+1]; }
+int tt_foncNbParam (int posFonc) { return getVal(posFonc+1); }
 
 /* Renvoie le numéro lexicographique du paramètre i (indexé à 0) de la fonction se 
    trouvant à posFonc */
-int tt_foncNumLexParam (tableType tt, int posFonc, int i) { return tt[posFonc+2+i*2]; }
+int tt_foncNumLexParam (int posFonc, int i) { return getVal(posFonc+2+i*2); }
 
 /* Renvoie le type du paramètre i (indexé à 0) de la fonction se trouvant à posFonc */
-int tt_foncTypeParam (tableType tt, int posFonc, int i) { return tt[posFonc+3+i*2]; }
+int tt_foncTypeParam (int posFonc, int i) { return getVal(posFonc+3+i*2); }
 
 
 
 /* Renvoie le nombre de paramètres de la procédure se trouvant à posProc */
-int tt_procNbParam (tableType tt, int posProc) { return tt[posProc]; }
+int tt_procNbParam (int posProc) { return getVal(posProc); }
 
 /* Renvoie le numéro lexicographique du paramètre i (indexé à 0) de la procédure se 
    trouvant à posProc */
-int tt_procNumLexParam (tableType tt, int posProc, int i) { return tt[posProc+1+i*2]; }
+int tt_procNumLexParam (int posProc, int i) { return getVal(posProc+1+i*2); }
 
 /* Renvoie le type du paramètre i (indexé à 0) de la procédure se trouvant à posProc */
-int tt_procTypeParam (tableType tt, int posProc, int i) { return tt[posProc+2+i*2]; }
+int tt_procTypeParam (int posProc, int i) { return getVal(posProc+2+i*2); }
 
 
+/* Affiche le tableau des types dans le terminal */
+void tt_afficher() {
+    int i;
 
-/* Libère la mémoire associé à la table des types  */
-void tt_detruire (tableType tt) {
-    if (tt != NULL) libere_mem(&tt);
+    printf("Table de représentation des types et des entêtes de sous-programmes :\n");
+    printf("[");
+    for (i = 0; i < longTabType+2 && i < T_TABLETYPES-1; i++) {
+        printf("%d, ", tt[i]);
+    }
+    if (i < T_TABLETYPES-1) {
+        printf("..., -1]\n");
+    } else printf("%d]\n", tt[i]);
 }
--- tst/tst_tabTypes.c
+++ tst/tst_tabTypes.c
@@ -1,62 +1,51 @@
 #include "tableTypes.h"
 #include <stdio.h>
 
-void afficherTabTypes (tableType tt, int l) {
-    int i;
-
-    printf("[");
-    for (i = 0; i < l-1; i++) {
-        printf("%d, ", tt[i]);
-    }
-    printf("%d]\n", tt[i]);
-}
-
 int main () {
-    tableType tt = tt_init();
-    int l = 0, pos;
+    int pos;
+
+    tt_init();
 
     /* Structures */
-    pos = tt_ajoutStruct(tt, &l, 2, 1, 2, 3, 4, 5, 6);
-    afficherTabTypes(tt, l);
+    pos = tt_ajoutStruct(2, 1, 2, 3, 4, 5, 6);
+    tt_afficher();
     /* Utilisation normale */
-    printf("nb champs struct1: %d\n", tt_structNbChamps(tt, pos));
-    pos = tt_ajoutStruct(tt, &l, 1, 7, 8, 9, 10, 11, 12);
-    afficherTabTypes(tt, l);
+    printf("nb champs struct1: %d\n", tt_structNbChamps(pos));
+    pos = tt_ajoutStruct(1, 7, 8, 9, 10, 11, 12);
+    tt_afficher();
     /* Ignore 10, 11, 12 */
-    printf("index champ 1 struct2: %d\n", tt_structIndexChamp(tt, pos, 0));
-    pos = tt_ajoutStruct(tt, &l, 1, 13, 14);
-    afficherTabTypes(tt, l);
+    printf("index champ 1 struct2: %d\n", tt_structIndexChamp(pos, 0));
+    pos = tt_ajoutStruct(1, 13, 14);
+    tt_afficher();
     /* Comportement imprévu */
-    printf("NumLex champ 1 struct3: %d\n", tt_structNumLexChamp(tt, pos, 0));
-    printf("Deplacement champ 2 struct1: %d\n", tt_structDeplacementChamp(tt, 0, 1));
+    printf("NumLex champ 1 struct3: %d\n", tt_structNumLexChamp(pos, 0));
+    printf("Deplacement champ 2 struct1: %d\n", tt_structDeplacementChamp(0, 1));
     
 
     /* Tableaux */
-    pos = tt_ajoutTab(tt, &l, 234, 2, 0, 5, 10, 15);
-    afficherTabTypes(tt, l);
-    printf("Type elements tab: %d\n", tt_tabTypeElem(tt, pos));
-    printf("Nb dimensions tab: %d\n", tt_tabNbDimensions(tt, pos));
-    printf("Borne inf dimension 1 tab: %d\n", tt_tabDimBornInf(tt, pos, 0));
-    printf("Borne sup dimension 2 tab: %d\n", tt_tabDimBornSup(tt, pos, 1));
+    pos = tt_ajoutTab(234, 2, 0, 5, 10, 15);
+    tt_afficher();
+    printf("Type elements tab: %d\n", tt_tabTypeElem(pos));
+    printf("Nb dimensions tab: %d\n", tt_tabNbDimensions(pos));
+    printf("Borne inf dimension 1 tab: %d\n", tt_tabDimBornInf(pos, 0));
+    printf("Borne sup dimension 2 tab: %d\n", tt_tabDimBornSup(pos, 1));
 
     /* Fonctions */
-    pos = tt_ajoutFonction(tt, &l, 1, 2, 2345, 2, 234, 1);
-    afficherTabTypes(tt, l);
-    printf("Type retour fonc: %d\n", tt_foncTypeRetour(tt, pos));
-    printf("Nb param fonc: %d\n", tt_foncNbParam(tt, pos));
-    printf("Num lex param 1 fonc: %d\n", tt_foncNumLexParam(tt, pos, 0));
-    printf("Type param 2 fonc: %d\n", tt_foncTypeParam(tt, pos, 1));
+    pos = tt_ajoutFonction(1, 2, 2345, 2, 234, 1);
+    tt_afficher();
+    printf("Type retour fonc: %d\n", tt_foncTypeRetour(pos));
+    printf("Nb param fonc: %d\n", tt_foncNbParam(pos));
+    printf("Num lex param 1 fonc: %d\n", tt_foncNumLexParam(pos, 0));
+    printf("Type param 2 fonc: %d\n", tt_foncTypeParam(pos, 1));
 
     /* Procédures */
-    pos = tt_ajoutProcedure(tt, &l, 2, 867, 3, 238, 0);
-    afficherTabTypes(tt, l);
-    printf("Nb param proc: %d\n", tt_procNbParam(tt, pos));
-    printf("Num lex param 1 proc: %d\n", tt_procNumLexParam(tt, pos, 0));
-    printf("Type param 2 proc: %d\n", tt_procTypeParam(tt, pos, 1));
+    pos = tt_ajoutProcedure(2, 867, 3, 238, 0);
+    tt_afficher();
+    printf("Nb param proc: %d\n", tt_procNbParam(pos));
+    printf("Num lex param 1 proc: %d\n", tt_procNumLexParam(pos, 0));
+    printf("Type param 2 proc: %d\n", tt_procTypeParam(pos, 1));
 
-    afficherTabTypes(tt, l+10);
-
-    tt_detruire(tt);
+    tt_afficher();
 
     exit(EXIT_SUCCESS);
 }
